78a201147a2e5ed4a61294889a879f34dc60fd6a
mhlinksfilterbot <107455088+mhlinksfilterbot@users.noreply.github.com>
2022-06-14 11:02:54 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -28,14 +28,14 @@ Bot Owner Only
 """
 
 ABOUT = """
-➥ Name :  Auto Filter V3
+➥ Name :  MH LINKS FIlTER BOT
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [@thilak2003](https://t.me/thilak2003)
+
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+
 """
